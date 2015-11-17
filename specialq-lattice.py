e9b9794d181077660171fed3396c3c6660de5ed2
paulocode <paulocode@paulocode.com>
2015-11-17 21:33:31 +0800

    messed up copy/pasting

--- specialq-lattice.py
+++ specialq-lattice.py
@@ -14,34 +14,6 @@ def main():
 	for i in range(2):
 		a = -b*r + i*q
 		basis.append([a,b])
-	
-
-	print "basis: %s" % basis
-	mat_basis = liblll.create_matrix(basis) 
-	mat_reduced = liblll.lll_reduction(mat_basis)
-	#mat_reduced = mat_basis # uncomment to not reduce lattice basis
-	a0 = mat_reduced[0][0].numerator # lll library creates fractions with denominator 1, so grab numerator
-	b0 = mat_reduced[0][1].numerator
-	a1 = mat_reduced[1][0].numerator
-	b1 = mat_reduced[1][1].numerator
-	print "reduced basis: %s" % [[a0,b0],[a1,b1]]
-	
-import poly
-import primemath
-import liblll
-def main():
-	nfspoly = poly.Poly([8,29,15,1])
-	NF = poly.NumberField(nfspoly)
-	q = 359
-	r = 110
-	if(nfspoly.evaluate(r)%q==0):
-		print "r is a root of f() mod q"
-	
-	basis = []
-	b = 1
-	for i in range(2):
-		a = -b*r + i*q
-		basis.append([a,b])
 
 	print "basis: %s" % basis
 	mat_basis = liblll.create_matrix(basis) 
